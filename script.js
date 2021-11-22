c45b4016de8a94d008371e6d1fbd8a7ecbafbd1b
Jorge Osses <jorgitoosses@gmail.com>
2021-11-22 01:41:51 -0300

    Courses with ajax

---
+++ script.js
@@ -0,0 +1,30 @@
+const url = "courses.json";
+const container = document.querySelector('.courses');
+
+fetch(url)
+.then((response) => response.json() )
+.then((data) => {
+    console.log(data)
+    data.forEach(element => {
+        let div = document.createElement('div');
+        div.className = 'responsive';
+        div.innerHTML =`
+            <div class="gallery">
+            <div class="desc">
+            <h4>${element.title}</h4>
+            <p>
+                ${element.place} <br>
+                Expedición: ${element.date} <br>
+                ID de la credencial: ${element.id}  <br>
+                <a href="${element.url}" target="_blank">Ver credencial</a>
+            </p>
+            </div>
+            </div>
+        `;
+        container.appendChild(div)
+    });
+
+})
+.catch((error) => {
+    console.log(error);
+})
